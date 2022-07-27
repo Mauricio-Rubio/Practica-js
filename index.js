e5b7d48680ece9ba73ccab0863ef5ea43d94b366
Mauricio-Rubio <mau.r.h54@gmail.com>
2022-07-26 21:02:48 -0500

    Arreglos y objetos

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 // Ejercicios
-
 // 1.- Muestra el primer elemento de un arreglo (solución hecha con clousures)
-function peek(){
-    let index = -1;
-    return function show(arr){
-        return arr[index+=1]
-    }
+function peek() {
+  let index = -1;
+  return function show(arr) {
+    return arr[(index += 1)];
+  };
 }
 const fElement = peek();
-console.log(fElement([1,29,90,30]));
+console.log(fElement([1, 29, 90, 30]));
 
 // 2.- Crea una función que reciba un array e imprima elemento por elemento
 // Solucion usando arrow function
-const show = (arr) => arr.forEach(element => {
+const showArr = (arr) =>
+  arr.forEach((element) => {
     console.log(element);
-}); 
-show([1,29,90,30]);
+  });
+showArr([1, 29, 90, 30]);
+
+// 3.- Crea una función que imprima cada elemento del objeto
+const mau = {
+  name: "Mauricio",
+  age: 18,
+  height: 1.85,
+  sport: 'soccer',
+  university: 'UNAM',
+};
+// Con Object keys
+const showObj = (obj) => {
+  let keys = Object.keys(obj);
+  for (let index = 0; index < keys.length; index++) {
+    console.log(obj[keys[index]]);
+  }
+};
+// Con Object values
+const showObjValues = (obj) => Object.values(obj).forEach((element) => {
+    console.log(element);
+});
+showObjValues(mau)
